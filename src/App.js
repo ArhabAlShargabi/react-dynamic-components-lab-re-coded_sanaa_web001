7723451be7efd663a7b1c62522db2981d553e4cf
Arhab Ahmed <arhab1000000@gmail.com>
2020-10-06 07:13:57 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
 
 class App extends Component {
+<<<<<<< HEAD
 
   // Nothing needs to change here! 
   // Make note of what prop we are initially passing to `ColorBox` below!
@@ -15,6 +16,20 @@ class App extends Component {
         <div className="wrapper">
           <ColorBox opacity={1} />
         </div>
+=======
+  
+  // Nothing needs to change here! 
+  // Make note of what prop we are initially passing to `ColorBox` below!
+  
+  render() {
+    return (
+      <div id="app">
+          <BlogPost />
+          <div id="seperator"></div>
+          <div className="wrapper">
+            <ColorBox opacity={1} />
+          </div>
+>>>>>>> dca8fc1cb6a743de804aaed394426a35fd5e3018
       </div>
     )
   }
